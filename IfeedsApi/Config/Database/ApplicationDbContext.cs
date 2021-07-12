2449a7c4a02d2337aa28e16b677d6ad72f22186f
RaphaelaJP <47481822+RaphaelaJP@users.noreply.github.com>
2021-07-11 21:48:31 -0300

    Populando tabelas: Campus, Usuario, Contato, FormularioAvaliacao, CampusFormularioAvaliacao

--- IfeedsApi/Config/Database/ApplicationDbContext.cs
+++ IfeedsApi/Config/Database/ApplicationDbContext.cs
@@ -34,6 +34,7 @@ namespace IfeedsApi.Config.Database
                 .HasIndex(p => p.Matricula)
                 .IsUnique();
 
+
             modelBuilder.Entity<Usuario>()
                 .Property(p => p.DataCriacao)
                 .HasDefaultValueSql("(utc_timestamp())");
@@ -53,6 +54,7 @@ namespace IfeedsApi.Config.Database
                 .HasIndex(p => p.Tipo)
                 .IsUnique();
 
+           
             modelBuilder.Entity<Role>()
                 .Property(p => p.DataCriacao)
                 .HasDefaultValueSql("(utc_timestamp())");
@@ -117,7 +119,58 @@ namespace IfeedsApi.Config.Database
                         Id = 2,
                         Email = "fulano@email.com",
                         Telefone = "79999999999"
-                    });
+                    },
+                    new
+                    {
+                        Id = 3,
+                        Email = "maria@email.com",
+                        Telefone = "71999999999"
+                    },
+                    new
+                    {
+                        Id = 4,
+                        Email = "jose@email.com",
+                        Telefone = "7199944444"
+                    },
+                    new
+                    {
+                        Id = 5,
+                        Email = "joao@email.com",
+                        Telefone = "7199933333"
+                    },
+                    new
+                    {
+                        Id = 6,
+                        Email = "carlos@email.com",
+                        Telefone = "71999995555"
+                    },
+                    new
+                    {
+                        Id = 7,
+                        Email = "rafaela@email.com",
+                        Telefone = "7199999999999"
+                    },
+                    new
+                    {
+                        Id = 8,
+                        Email = "kaiki@email.com",
+                        Telefone = "71999666666",
+                    },
+                    new
+                    {
+                        Id = 9,
+                        Email = "vanilton@email.com",
+                        Telefone = "71999977777",
+                    },
+                    new
+                    {
+                        Id = 10,
+                        Email = "chico@email.com",
+                        Telefone = "71999998888",
+                    }
+                 );
+
+
 
             modelBuilder.Entity<Role>()
                 .HasData(
@@ -133,17 +186,267 @@ namespace IfeedsApi.Config.Database
                     }
                 );
 
+
+
+
             modelBuilder.Entity<Usuario>()
                 .HasData(
-                    new
+                    new 
                     {
                         Id = 1,
                         Nome = "Cicrano",
                         Matricula = "20211897238",
                         ContatoId = 1,
                         RoleId = 1
+                    },
+                    new 
+                    {
+                        Id = 2,
+                        Nome = "Fulano",
+                        Matricula = "21203554",
+                        ContatoId = 2,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 3,
+                        Nome = "Maria",
+                        Matricula = "20215888854",
+                        ContatoId = 3,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 4,
+                        Nome = "Jose",
+                        Matricula = "2021777554",
+                        ContatoId = 4,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 5,
+                        Nome = "Joao",
+                        Matricula = "2021533554",
+                        ContatoId = 5,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 6,
+                        Nome = "Carlos",
+                        Matricula = "20215554",
+                        ContatoId = 6,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 7,
+                        Nome = "Rafaela",
+                        Matricula = "20213554",
+                        ContatoId = 7,
+                        RoleId = 2
+                    },
+                    new
+                    {
+                        Id = 8,
+                        Nome = "Kaiki",
+                        Matricula = "202156954",
+                        ContatoId = 8,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 9,
+                        Nome = "Vanilton",
+                        Matricula = "20266654",
+                        ContatoId = 9,
+                        RoleId = 2
+                    },
+                    new 
+                    {
+                        Id = 10,
+                        Nome = "Chico",
+                        Matricula = "2021585641",
+                        ContatoId = 10,
+                        RoleId = 2
                     }
                 );
+
+
+
+            modelBuilder.Entity<Campus>()
+                .HasData(
+                    new
+                    {
+                        Id = 1,
+                        Nome = "IFS-Campus Lagarto",
+                    },
+
+                    new
+                    {
+                        Id = 2,
+                        Nome = "IFS-Campus Glória",
+                    },
+
+                    new
+                    {
+                        Id = 3,
+                        Nome = "IFS-Campus Estância",
+                    },
+                    new
+                    {
+                        Id = 4,
+                        Nome = "IFS-Campus São Cristovão",
+                    },
+                    new
+                    {
+                        Id = 5,
+                        Nome = "IFS-Campus Itabaiana",
+                    },
+                    new
+                    {
+                        Id = 6,
+                        Nome = "IFS-Campus Aracaju",
+                    },
+                    new
+                    {
+                        Id = 7,
+                        Nome = "IFS-Campus Tobias Barreto",
+                    },
+                    new
+                    {
+                        Id = 8,
+                        Nome = "IFS-Campus Poço Redondo",
+                    },
+                    new
+                    {
+                        Id = 9,
+                        Nome = "IFS-Campus Propriá",
+                    },
+                    new
+                    {
+                        Id = 10,
+                        Nome = "IFS-Campus Socorro",
+                    },
+                    new
+                    {
+                        Id = 11,
+                        Nome = "IFS-Educação a Distancia",
+                    }
+
+                );
+
+
+
+
+            modelBuilder.Entity<FormularioAvaliacao>()
+                .HasData(
+                    new
+                    {
+                        Id = 1,
+                        Descricao = "Banheiro com problema espelho quebrado",
+                        QuantidadeEstrelas = 3.50m,
+                    },
+                    new
+                    {
+                        Id = 2,
+                        Descricao = "Corrimão da Escada da COINF Descascando. Me cortei",
+                        QuantidadeEstrelas = 3m,
+                    },
+                    new
+                    {
+                        Id = 3,
+                        Descricao = "Demora para Aprovar Máteria de TCC 1 - Estou com mais de um mes aguardando aprovação",
+                        QuantidadeEstrelas = 2.50m,
+                    },
+                    new
+                    {
+                        Id = 4,
+                        Descricao = "Melhorias no Processo de Atendimento -  Deveriam automatizar mais os processos",
+                        QuantidadeEstrelas = 3m,
+                    },
+                    new
+                    {
+                        Id = 5,
+                        Descricao = "Pessoal da Limpeza Top - Campus sempre muito bem conservado",
+                        QuantidadeEstrelas = 5m,
+                    },
+                    new
+                    {
+                        Id = 6,
+                        Descricao = "Problemas com o Wifi do Campus - Internet lenta o que dificulta o acesso",
+                        QuantidadeEstrelas = 4m,
+                    },
+
+                    new
+                    {
+                        Id = 7,
+                        Descricao = "Ifs Techday Top - Evento maravilhoso",
+                        QuantidadeEstrelas = 5m,
+                    },
+                    new
+                    {
+                        Id = 8,
+                        Descricao = "Semana Academica Divertida - Evento top",
+                        QuantidadeEstrelas = 4.50m,
+                    },
+                    new
+                    {
+                        Id = 9,
+                        Descricao = "Semana Academica Algumas Palestras Massantes - palestras muito longas e cansativas",
+                        QuantidadeEstrelas = 2.50m,
+                    },
+                    new
+                    {
+                        Id = 10,
+                        Descricao = "Lab da COINF precisão limpar o filtro do Ar - nunca funciona corretamente",
+                        QuantidadeEstrelas = 3.50m,
+                    },
+                    new
+                    {
+                        Id = 11,
+                        Descricao = "Lab de Redes com acustica prejudicada - Barulho da oficina atrapalha a aula",
+                        QuantidadeEstrelas = 2.50m,
+                    },
+                    new
+                    {
+                        Id = 12,
+                        Descricao = "Nenhum Livro de Loiane Groner - acervos desatualizados",
+                        QuantidadeEstrelas = 3m,
+                    },
+                    new
+                    {
+                        Id = 13,
+                        Descricao = "Livro de Cálculo com página rasurada - o livro está com rasuras",
+                        QuantidadeEstrelas = 3m,
+                    }
+            );
+
+
+            modelBuilder
+                .Entity<Campus>()
+                .HasMany(p => p.FormulariosAvaliacoes)
+                .WithMany(p => p.Campus)
+                .UsingEntity(j => j.ToTable("CampusFormularioAvaliacao")
+                );
+
+            modelBuilder
+                .Entity<Campus>()
+                .HasMany(p => p.FormulariosAvaliacoes)
+                .WithMany(p => p.Campus)
+                .UsingEntity(j => j.HasData(
+                    new { CampusId = 1, FormulariosAvaliacoesId = 1 },
+                    new { CampusId = 2, FormulariosAvaliacoesId = 2 },
+                    new { CampusId = 3, FormulariosAvaliacoesId = 4 },
+                    new { CampusId = 4, FormulariosAvaliacoesId = 3 },
+                    new { CampusId = 5, FormulariosAvaliacoesId = 5 },
+                    new { CampusId = 6, FormulariosAvaliacoesId = 7 },
+                    new { CampusId = 7, FormulariosAvaliacoesId = 6 },
+                    new { CampusId = 8, FormulariosAvaliacoesId = 8 }, 
+                    new { CampusId = 9, FormulariosAvaliacoesId = 10 }, 
+                    new { CampusId = 10,FormulariosAvaliacoesId = 9}
+                ));
 
         }
     }
