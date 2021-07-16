66afa3ed2278c8ea60ea9ed89a0a35196b6eea0b
vanilton filho <vanilton.alv3sfilho@gmail.com>
2021-07-16 12:40:53 -0300

    Corrigindo último merge e refazendo as migrations

--- IfeedsApi/Migrations/20210716153848_MigrationInicial_v2.Designer.cs
+++ IfeedsApi/Migrations/20210716153848_MigrationInicial_v2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using IfeedsApi.Config.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IfeedsApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210716153848_MigrationInicial_v2")]
+    partial class MigrationInicial_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
